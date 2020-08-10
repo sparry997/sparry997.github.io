2a833e58dc742807f90a277e4375f835649e8cd9
Spencer Parry <sparry997@gmail.com>
2020-08-10 14:33:12 -0500

    More changes

--- sparry997.github.io/src/app/app-routing.module.ts
+++ sparry997.github.io/src/app/app-routing.module.ts
@@ -4,12 +4,18 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule),
+    data: {animation: 'Home'}
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },
+  {
+    path: 'about',
+    loadChildren: () => import('./about/about.module').then( m => m.AboutPageModule),
+    data: {animation: 'About'}
   },
 ];
 
--- sparry997.github.io/src/app/app.module.ts
+++ sparry997.github.io/src/app/app.module.ts
@@ -9,11 +9,13 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, BrowserAnimationsModule],
+  imports: [BrowserModule, BrowserAnimationsModule, IonicModule.forRoot(), AppRoutingModule, BrowserAnimationsModule, NgbModule],
   providers: [
     StatusBar,
     SplashScreen,
